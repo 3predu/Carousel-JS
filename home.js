3499b5da1daf57bdb1a975ef1cbcf6accdd82e91
3predu <3pedrolucca@gmail.com>
2021-06-05 07:24:57 -0300

    carousel commit

---
+++ home.js
@@ -0,0 +1,32 @@
+window.load = slide(1);
+
+var bgNumber = 1;
+
+function slide(n){
+    let allBgs = 4;
+    document.getElementById('img-back').style.backgroundImage = "url('images/"+n+".jpg')";
+    botoes(n, allBgs);
+    bgNumber = n;
+}
+function btanterior(){
+    if(bgNumber > 1){
+        bgNumber--;
+        slide(bgNumber);
+    }
+}
+function btposterior(){
+    if(bgNumber < 4){
+        bgNumber++;
+        slide(bgNumber);
+    }
+}
+function botoes(n, m){
+    document.getElementById('botoes').innerHTML = "";
+    for(a = 1; a <= m; a++){
+        if(a == n){
+            document.getElementById('botoes').innerHTML += "<li class='selected' onclick='slide("+a+")'></li>";
+        }else{
+            document.getElementById('botoes').innerHTML += "<li onclick='slide("+a+")'></li>";
+        }
+    }
+}
